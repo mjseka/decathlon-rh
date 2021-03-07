f364bc11d225b3584d9c522283ebeb5034b405d2
mjseka <80000441+mjseka@users.noreply.github.com>
2021-03-07 17:41:15 +0000

    Add files via upload

---
+++ dbconnect.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+try 
+{
+    $db = new PDO('mysql:host=localhost; dbname=decathlon; charset=utf8', 'root', '');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e)
+{
+    //echo "Connection failed:".$e->getMessage();
+}
+?>
